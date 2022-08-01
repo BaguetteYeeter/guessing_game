3c77eebe7e0cc059bc6f8f2c57d9c994b202bb4e
BaguetteYeeter <baguetteyeeter@gmail.com>
2022-08-01 19:02:48 +0200

    fixed and added comments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::io;
+use std::io; //import io from standard library
+use std::io::Write; //import print stuff
 
-fn main() {
-    println!("Guess the number!");
+fn main() { //define the function main
+    println!("Guess the number!"); //print
 
-    println!("Please input your guess.");
+    print!("Please input your guess. "); //another print
+    io::stdout().flush().expect("Failed to flush"); //dont add new line
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //make a new mutable string
 
-    io::stdout().flush();
+    io::stdin()                         //get user input
+        .read_line(&mut guess)          //store in guess variable
+        .expect("Failed to read line"); //send error message
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess}"); //prints user guess
 }
