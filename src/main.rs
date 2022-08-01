66aa9e0e7314c9396e8e7245b81eaa166a8c324d
BaguetteYeeter <baguetteyeeter@gmail.com>
2022-08-01 19:22:20 +0200

    added loop

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,24 @@ fn main() { //define the function main
 
     println!("The secret number is: {secret_number}"); //print secret number
 
-    print!("Please input your guess. "); //another print
-    io::stdout().flush().expect("Failed to flush"); //dont add new line
+    loop { //make a new while true loop
+        print!("Please input your guess. "); //another print
+        io::stdout().flush().expect("Failed to flush"); //dont add new line
 
-    let mut guess = String::new(); //make a new mutable string
+        let mut guess = String::new(); //make a new mutable string
 
-    io::stdin()                         //get user input
-        .read_line(&mut guess)          //store in guess variable
-        .expect("Failed to read line"); //send error message
+        io::stdin()                         //get user input
+            .read_line(&mut guess)          //store in guess variable
+            .expect("Failed to read line"); //send error message
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); //convert string to int
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); //convert string to int
 
-    println!("You guessed: {guess}"); //prints user guess
+        println!("You guessed: {guess}"); //prints user guess
 
-    match guess.cmp(&secret_number) { //compare the two numbers
-        Ordering::Less => println!("Too small!"), //if guess is too big print that
-        Ordering::Greater => println!("Too big!"), //if guess is too small print that
-        Ordering::Equal => println!("You win!"), //if same as guess print that
-    } //end match
+        match guess.cmp(&secret_number) { //compare the two numbers
+            Ordering::Less => println!("Too small!"), //if guess is too big print that
+            Ordering::Greater => println!("Too big!"), //if guess is too small print that
+            Ordering::Equal => println!("You win!"), //if same as guess print that
+        } //end match
+    } //end loop
 } //end main
